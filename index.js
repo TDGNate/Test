64535bb3b2b920c5f3ce209db2c13bc3294cc02c
Nate <itsnzte@gmail.com>
2022-08-23 04:50:07 -0700

    touched up all files, and readjusted hero img

---
+++ index.js
@@ -0,0 +1,18 @@
+VANTA.BIRDS({
+  el: ".hero",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 300.00,
+  minWidth: 300.00,
+  scale: 1.00,
+  scaleMobile: 2.00,
+  birdSize: 0.81,
+  wingSpan: 3.00,
+  speedLimit: 0.90,
+  separation: 90.00,
+  alignment: 12.00,
+  cohesion: 55.50,
+  quantity: 0.50,
+  backgroundAlpha: 0.00
+})

61e41c654b3928f35b4a0bef549ebafe1bfe7034
Nate <itsnzte@gmail.com>
2022-08-29 00:47:27 -0700

    testing EmailJS forms

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-VANTA.BIRDS({
-  el: ".hero",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 100.00,
-  minWidth: 100.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  birdSize: 0.93,
-  wingSpan: 3.00,
-  speedLimit: 2.80,
-  separation: 90.00,
-  alignment: 12.00,
-  cohesion: 55.50,
-  quantity: 1.50,
-  backgroundAlpha: 0.00
-})
+window.onload = function () {
+  const btn = document.getElementById('send')
+  document
+    .getElementById('contact-form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('service_k2icfbo', 'template_crd40yf', this)
+        .then(() => {
+          btn.value = 'Sending...';
+          console.log('SUCCESS!');
+        }, (error) => {
+          btn.value = 'Send';
+          console.log('FAILED...', error);
+        },
+      )
+    });
+};
---
+++ pop/index.js
@@ -0,0 +1,18 @@
+VANTA.BIRDS({
+  el: ".hero",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 100.00,
+  minWidth: 100.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  birdSize: 0.93,
+  wingSpan: 3.00,
+  speedLimit: 2.80,
+  separation: 90.00,
+  alignment: 12.00,
+  cohesion: 55.50,
+  quantity: 1.50,
+  backgroundAlpha: 0.00
+})
